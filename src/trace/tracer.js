3aba267a5d82d1c30e8031ad354c2962263b1fbb
meliodas137 <mayankgt1043@gmail.com>
2023-04-14 19:56:31 -0400

    added js tracing generation and modifyied it to dump in log file

---
+++ src/trace/tracer.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const opentelemetry = require('@opentelemetry/api');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { FileTraceExporter } = require("opentelemetry-exporter-trace-otlp-file");
+const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+
+const EXPORTER = process.env.EXPORTER || '';
+
+module.exports = (serviceName) => {
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  let exporter;
+  if (EXPORTER.toLowerCase().startsWith('z')) {
+    exporter = new ZipkinExporter();
+  } else if(EXPORTER.toLowerCase().startsWith('j')) {
+    exporter = new JaegerExporter();
+  } else {
+    exporter = new FileTraceExporter({ filePath: "./logs/trace.json" });
+  }
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+  // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
+  provider.register();
+
+  registerInstrumentations({
+    // // when boostraping with lerna for testing purposes
+    instrumentations: [
+      new HttpInstrumentation(),
+    ],
+  });
+
+  return opentelemetry.trace.getTracer('http-example');
+};
