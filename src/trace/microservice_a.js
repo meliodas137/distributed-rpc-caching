4057b1b4b231e7741186c79beaef0a62fe183f9d
meliodas137 <mayankgt1043@gmail.com>
2023-04-16 19:56:55 -0400

    removing loops

--- src/trace/microservice_a.js
+++ src/trace/microservice_a.js
@@ -26,14 +26,13 @@ function makeRequest(url = '') {
   });
 }
 
-function loopRequests(loop = 1){
-  for(var i = 0; i < loop; i+=1){
-    console.log('Sleeping 1 seconds after every request');
-    setTimeout(() => { makeRequest('/s1'); console.log('Completed.'); }, 1000);
-    setTimeout(() => { makeRequest('/s2'); console.log('Completed.'); }, 2000);
-    setTimeout(() => { makeRequest('/s1'); console.log('Completed.'); }, 3000);
-    setTimeout(() => { makeRequest(); console.log('Completed.'); }, 4000);
-  }
+function doRequests(){
+  console.log('Sleeping 1 seconds after every request');
+  setTimeout(() => { makeRequest('/s1'); console.log('Completed.'); }, 500);
+  setTimeout(() => { makeRequest('/s2'); console.log('Completed.'); }, 1500);
+  setTimeout(() => { makeRequest('/s3'); console.log('Completed.'); }, 2000);
+  setTimeout(() => { makeRequest('/s4'); console.log('Completed.'); }, 2500);
+  setTimeout(() => { makeRequest(); console.log('Completed.'); }, 3000);
 }
 
-loopRequests();
+doRequests();
