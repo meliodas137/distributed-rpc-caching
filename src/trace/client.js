605abe5d9083e5bed6b8f235678580ef9fbe291b
meliodas137 <mayankgt1043@gmail.com>
2023-04-16 18:03:59 -0400

    calling multiple urls from client

--- src/trace/client.js
+++ src/trace/client.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const api = require('@opentelemetry/api');
-const tracer = require('./tracer')('example-http-client');
+const tracer = require('./tracer')('microservice-one');
 const http = require('http');
 
 /** A function which makes requests and handles response. */
-function makeRequest() {
+function makeRequest(url = '') {
   // span corresponds to outgoing requests. Here, we have manually created
   // the span, which is created to track work that happens outside of the
   // request lifecycle entirely.
@@ -13,22 +13,23 @@ function makeRequest() {
     http.get({
       host: 'localhost',
       port: 8080,
-      path: '/helloworld',
+      path: url,
     }, (response) => {
       const body = [];
       response.on('data', (chunk) => body.push(chunk));
       response.on('end', () => {
+        span.setAttribute("service.output", decodeURIComponent(body.toString()));
         console.log(body.toString());
         span.end();
       });
     });
   });
-
-  // The process must live for at least the interval past any traces that
-  // must be exported, or some risk being lost if they are recorded after the
-  // last export.
-  console.log('Sleeping 5 seconds before shutdown to ensure all records are flushed.');
-  setTimeout(() => { console.log('Completed.'); }, 5000);
 }
 
+makeRequest('/s1');
+makeRequest('/s2');
+makeRequest('/s3');
 makeRequest();
+
+console.log('Sleeping 5 seconds before shutdown to ensure all records are flushed.');
+setTimeout(() => { console.log('Completed.'); }, 5000);
