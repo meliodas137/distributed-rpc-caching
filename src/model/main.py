24f988b53414a9e7c378dc05c0dea945b3e6a83d
utkarshtyagi <ut2028@nyu.edu>
2023-04-20 18:47:58 -0400

    Basic Parsing
    resolved conflicts and merged main

--- src/model/main.py
+++ src/model/main.py
@@ -2,40 +2,55 @@ import sys
 sys.path.append('../parser')
 
 from mm import MysteryMachine
-from graph import Graph
+from graph import Graph, NodeGraph
 from parser.data_struct import Instruction
-from parser.node import InputComponents
+from parser.node import InputComponents, Node
+
+def generateNodes(components):
+    nodes, mark = [], ["Apple"]
+    c = 0
+    for component in components:
+        node = Node(c, component)
+        if component in mark:
+            node.deterministic = False
+        nodes.append(node)
+        c = c+1
+    return nodes
 
 def mainSolver():
     input = InputComponents()
-    input.findInputComponents()
-    print(input.components)
-    print(input.componentIndexMap)
-    for instr in input.instructions:
-        print(instr)
-    for node in input.nodes:
-        print(node)
-    used, notUsed = 0, 0
-    for observation in input.observationCollection.observations:
-        if observation.used:
-            used = used + 1
-        else:
-            print(observation)
-            notUsed = notUsed + 1
-    print(used)
-    print(notUsed)
-    print(len(input.instructions))
+    # input.findInputComponents()
+    # print(input.components)
+    # print(input.componentIndexMap)
+    # for instr in input.instructions:
+    #     print(instr)
+    # for node in input.nodes:
+    #     print(node)
+    # used, notUsed = 0, 0
+    # for observation in input.observationCollection.observations:
+    #     if observation.used:
+    #         used = used + 1
+    #     else:
+    #         print(observation)
+    #         notUsed = notUsed + 1
+    # print(used)
+    # print(notUsed)
+    # print(len(input.instructions))
     
 
-    # input.components = ["Apple", "Banana", "Cucumber", "Peach"]
-    # input.instructions = [
-    #     Instruction(caller="Apple", callee="Banana"),
-    #     Instruction(caller="Cucumber", callee="Peach"),
-    #     Instruction(caller="Banana", callee="Cucumber"),
-    #     Instruction(caller="Apple", callee="Banana"),
-    #     Instruction(caller="Apple", callee="Banana"),
-    #     Instruction(caller="Cucumber", callee="Peach")
-    # ]
+    input.components = ["Eatables", "Meat", "Fruit", "Apple", "Veggies", "Brocolli"]
+    input.instructions = [
+        Instruction(caller="Eatables", callee="Meat"),
+        Instruction(caller="Eatables", callee="Fruit"),
+        Instruction(caller="Eatables", callee="Veggies"),
+        Instruction(caller="Eatables", callee="Meat"),
+        Instruction(caller="Eatables", callee="Fruit"),
+        Instruction(caller="Fruit", callee="Apple"),
+        Instruction(caller="Fruit", callee="Apple"),
+        Instruction(caller="Veggies", callee="Brocolli"),
+        Instruction(caller="Veggies", callee="Brocolli"),
+        Instruction(caller="Eatables", callee="Meat")
+    ]
     # input.components = ["a", "b", "c"]
     # input.instructions = [
     #     Instruction(caller="a", callee="b"),
@@ -45,17 +60,23 @@ def mainSolver():
     #     Instruction(caller="a", callee="c"),
     #     Instruction(caller="a", callee="c")
     # ]
+    input.nodes = generateNodes(input.components)
     mysteryMachine = MysteryMachine(input.components, input.instructions)
     mysteryMachine.constructModel()
     print(mysteryMachine.componentMap)
     print(mysteryMachine.causalGraph)
     print(mysteryMachine.observationGraph)
-    print("Paths")
+    print("All Models")
     mysteryMachine.EnumeratePaths()
     print("Chosen Model")
-    model: Graph = mysteryMachine.chooseGraphModel()
+    graph: Graph = mysteryMachine.chooseGraphModel()
+    print(graph)
+    print(graph.printLabelledGraph(input.components))
+    model = NodeGraph(input.nodes, graph)
     print(model)
-    print(model.printLabelledGraph(input.components))
+    sets = mysteryMachine.generateAllSets([1,2,3])
+    print(sets)
+    print(sets[7])
 
 if __name__ == '__main__':
     mainSolver()
