30bbd46de1d495cfafdfeec0ffd06f3f565da34b
utkarshtyagi <ut2028@nyu.edu>
2023-04-21 14:08:56 -0400

    Working MM code

--- src/model/main.py
+++ src/model/main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../parser')
 
 from mm import MysteryMachine
+from graph import Graph
 from parser.data_struct import Instruction
 from parser.node import InputComponents
 
@@ -12,28 +13,37 @@ def mainSolver():
     print(input.componentIndexMap)
     for instr in input.instructions:
         print(instr)
-    # for node in input.nodes:
-    #     print(node)
+    for node in input.nodes:
+        print(node)
     used, notUsed = 0, 0
     for observation in input.observationCollection.observations:
         if observation.used:
             used = used + 1
         else:
-            # print(observation)
+            print(observation)
             notUsed = notUsed + 1
     print(used)
     print(notUsed)
     print(len(input.instructions))
     
 
-    # input = ["Apple", "Banana", "Cucumber", "Peach"]
-    # instructions = [
+    # input.components = ["Apple", "Banana", "Cucumber", "Peach"]
+    # input.instructions = [
     #     Instruction(caller="Apple", callee="Banana"),
     #     Instruction(caller="Cucumber", callee="Peach"),
     #     Instruction(caller="Banana", callee="Cucumber"),
     #     Instruction(caller="Apple", callee="Banana"),
     #     Instruction(caller="Apple", callee="Banana"),
     #     Instruction(caller="Cucumber", callee="Peach")
+    # ]
+    # input.components = ["a", "b", "c"]
+    # input.instructions = [
+    #     Instruction(caller="a", callee="b"),
+    #     Instruction(caller="a", callee="c"),
+    #     Instruction(caller="a", callee="b"),
+    #     Instruction(caller="a", callee="b"),
+    #     Instruction(caller="a", callee="c"),
+    #     Instruction(caller="a", callee="c")
     # ]
     mysteryMachine = MysteryMachine(input.components, input.instructions)
     mysteryMachine.constructModel()
@@ -42,6 +52,10 @@ def mainSolver():
     print(mysteryMachine.observationGraph)
     print("Paths")
     mysteryMachine.EnumeratePaths()
+    print("Chosen Model")
+    model: Graph = mysteryMachine.chooseGraphModel()
+    print(model)
+    print(model.printLabelledGraph(input.components))
 
 if __name__ == '__main__':
     mainSolver()
--- src/model/mm.py
+++ src/model/mm.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../')
 
 import numpy as np
+from graph import Graph
 from parser.data_struct import Instruction
 
 class MysteryMachine:
@@ -14,31 +15,29 @@ class MysteryMachine:
         self.causalGraph = []
         self.observationGraph = []
         self.causalPaths = []
-
+        self.modelGraphs: Graph = []
+        self.graphSet = set()
+    
 
     def constructModel(self):
         self.__initializeDAG()
         self.__executeInstructions()
 
-
     def __initializeDAG(self):
         for i, string_value in enumerate(self.components):
             self.componentMap[string_value] = i
-            self.componentIdToLabelMap[i] = string_value
-            
+            self.componentIdToLabelMap[i] = string_value     
         n = len(self.components)
         self.causalGraph = np.ones((n, n), dtype=int)
         for i in range(n):
             self.causalGraph[i][i] = 0
         self.observationGraph = np.zeros((n, n), dtype=int)
 
-
     def __executeInstructions(self):
         for instruction in self.instructions:
             self.__executeInstruction(instruction)
-            
-
-    def __executeInstruction(self, instruction):
+        
+    def __executeInstruction(self, instruction: Instruction):
         callerId = self.componentMap[instruction.caller]
         calleeId = self.componentMap[instruction.callee]
         self.causalGraph[calleeId][callerId] = 0
@@ -47,60 +46,58 @@ class MysteryMachine:
     def EnumeratePaths(self):
         n = len(self.components)
         nodeAdded = np.zeros(n, dtype=int)
-        for i in range(n):
-            nodeAdded[i] = 1
-            for j in range(n):
-                currentPath = [i]
-                if self.causalGraph[i][j] == 1:
-                    currentPath.append(j)
-                    nodeAdded[j] = 1
-                    self.__explorePaths(currentPath, j, n, nodeAdded)
-            nodeAdded[i] = 0
+        for index in range(n):
+            nodeAdded[index] = 1
+            adj = [[] for j in range(n)]
+            self.__explorePaths(adj, index, index, n, nodeAdded, 0)
+            nodeAdded[index] = 0
         
         self.__printPaths()
         print("Filtered Paths:")
         self.__filterPaths()
+        self.__printPaths()
 
-    def __explorePaths(self, currentPath, currentIndex, maxIndex, nodeAdded):
+    def __explorePaths(self, adj, currentIndex, parentIndex, maxIndex, nodeAdded, score):
+        # print(str(currentIndex) + ", " + str(parentIndex) + " -> " + str(nodeAdded))
         notLeaf = False
-        for i in range(maxIndex):
-            if self.causalGraph[currentIndex][i] == 0 or nodeAdded[i] == 1:
-                continue
-            notLeaf = True
-            currentPath.append(i)
-            nodeAdded[i] = 1
-            self.__explorePaths(currentPath, i, maxIndex, nodeAdded)
+        for mode in range(2):
+            for neighbor in range(maxIndex):
+                if self.causalGraph[currentIndex][neighbor] == 0 or nodeAdded[neighbor] == 1:
+                    continue
+                notLeaf = True
+                edgeScore = self.observationGraph[currentIndex][neighbor]
+                adj[currentIndex].append(neighbor)
+                # print(f"mode={mode}, current={currentIndex}, neighbor={neighbor}, nodeAdded={nodeAdded}")
+                nodeAdded[neighbor] = 1
+                if mode == 0:
+                    self.__explorePaths(adj, currentIndex, currentIndex, maxIndex, nodeAdded, score + edgeScore)
+                else:
+                    self.__explorePaths(adj, neighbor, currentIndex, maxIndex, nodeAdded, score+edgeScore)
+                nodeAdded[neighbor] = 0
+                adj[currentIndex].remove(neighbor)
         
         if not notLeaf:
-            self.causalPaths.append(list(currentPath))
-        
-        nodeAdded[currentIndex] = 0
-        currentPath.pop()
+            graph = Graph(maxIndex - 1, adj, score)
+            strValue = graph.getStrAdj()
+            if strValue not in self.graphSet:
+                self.graphSet.add(strValue)
+                self.modelGraphs.append(graph)
+                # print("Graph added: " + str(adj))
+            else:
+                # print("Duplicate Graph: " + str(adj))
+                pass
+
+    def __printPaths(self):
+        for graph in self.modelGraphs:
+            print(graph)
 
     def __filterPaths(self):
-        filterPaths = []
-        for path in self.causalPaths:
-            score = self.__getPathScore(path)
-            if score > 0:
-                filterPaths.append(path)
-            else:
-                print("Filtered: " + str(path))
-        self.causalPaths = filterPaths
-        self.__printPaths()
-    
-    def __getPathScore(self, path):
-        count = 0
-        for index in range(len(path) - 1):
-            if self.observationGraph[path[index]][path[index+1]] > 0:
-                count += self.observationGraph[path[index]][path[index+1]]
-        return count
+        graphs = []
+        for graph in self.modelGraphs:
+            if graph.score != 0:
+                graphs.append(graph)
+        self.modelGraphs = graphs
 
-    
-    def __printPaths(self):
-        print(len(self.causalPaths))
-        for path in self.causalPaths:
-            print(self.__getLabelledPath(path))
-    
     def __getLabelledPath(self, path):
         pathString = ""
         for node in path:
@@ -108,3 +105,12 @@ class MysteryMachine:
             # pathString = pathString + str(node) + " -> "
         pathString = pathString + str(self.__getPathScore(path))
         return pathString
+    
+    def chooseGraphModel(self):
+        score = -1
+        maxGraph = None
+        for graph in self.modelGraphs:
+            if graph.score > score:
+                score = graph.score
+                maxGraph = graph
+        return maxGraph
