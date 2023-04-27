511ab0b3f8300dd7bc17cae69d2440148299dfe8
utkarshtyagi <ut2028@nyu.edu>
2023-04-27 19:19:28 -0400

    Cache file generation code

--- src/parser/main.py
+++ src/parser/main.py
@@ -4,7 +4,9 @@ import json
 def mainSolver():
     input = InputComponents()
     input.findInputComponents()
-    print(json.dumps(input.nodeCaches, cls=JsonEncoder, indent=4))
+    # print(json.dumps(input.nodeCaches, cls=JsonEncoder, indent=4))
+    with open("../trace/cacheInfo.json", "w") as f:
+        json.dump(input.nodeCaches, f, cls=JsonEncoder)
 
 if __name__ == '__main__':
     mainSolver()
